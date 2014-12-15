ec31789cb69a6596355127bed7677680aeb737b2
Jens Vangindertael <NiteLite@Jenss-MacBook-Pro.local>
2014-12-15 03:04:30 +0100

    TMR0, TMR1 working and generate overflows. Timer1 shifted to have 100Hz exactly. Switches are also read and put in NEW register. FSM structure still has to be implimented.

--- Microprocessors Lab 4 FSM.X/FSM.asm
+++ Microprocessors Lab 4 FSM.X/FSM.asm
@@ -8,8 +8,11 @@
 	list p=18F2550, r=hex, n=0
 	#include	<p18F2550.inc>
 
-NEW    equ 0x21
-OLD    equ 0x22
+NEW         equ 0x21
+OLD         equ 0x22
+SHIFTHIGH   equ 0x23
+SHIFTLOW    equ 0x24
+FLAG        equ 0x25
 
 
 ;***********************************************************
@@ -25,9 +28,9 @@ OLD    equ 0x22
 ; 					    Interrupt Vector
 ;***********************************************************
 
-	ORG     0x008	; Interrupt Vector  HIGH priority
+	ORG     0x008       ; Interrupt Vector  HIGH priority
 	GOTO	inter_high	; When debugging:0x008; when loading: 0x808
-    ORG     0x018	; Interrupt Vector  HIGH priority
+    ORG     0x018       ; Interrupt Vector  HIGH priority
 	GOTO	inter_low	; When debugging:0x008; when loading: 0x808
 
 ;***********************************************************
@@ -67,35 +70,89 @@ START
     BSF     INTCON, PEIE    ; Peripheral interrupts enabled
     BSF     INTCON, TMR0IE  ; Timer 0 interrupt enabled
 	BSF		PIE1,  TMR1IE   ; Timer 1 interrupt enabled
+    BSF     PIE2,  TMR3IE   ; Timer 3 interrupt enabled
+
     BSF     T0CON, TMR0ON   ; Timer 0 ON
 	BSF		T1CON, TMR1ON	; Timer 1 ON
+    BCF     T3CON, TMR3ON   ; Timer 3 ON
+
+    BSF     T0CON, T0PS1    ; Prescaler tmr0 @ 1:16
+    BSF     T0CON, T0PS0    ; 12Mhz/(256*256*16) = 11Hz (good enough)
+    BCF     T1CON, T1CKPS1  ; Prescaler tmr1 @ 1:2
+    BSF     T1CON, T1CKPS0  ; 12Mhz/(256*256*2) = 91Hz (around 100Hz)
+                            ; Nog aanpassen zodat exact 100Hz
 	CLRF	RCON			; Interrupt priority --> All high
 
     ;Initialisation
 
-    clrf    WREG
+    movlw   0x15            ; predetermined values to let
+    movwf   SHIFTHIGH       ; timer1 interrupt at exactly 100 Hz
+    movlw   0xA0            ; 12Mhz/(2*60000) = 100Hz so we have to
+    movwf   SHIFTLOW        ; shift timer1 5536 up = 0X15A0 (see TMR1_init)
 
-    
+    clrf    WREG
 
 MAIN
 
-    CALL FSM
-    GOTO MAIN
+    BTFSC   FLAG,0          ;Timer0 overflowed flag set
+    CALL    Update_SW
+    GOTO    Stepper_FSM
+
+Update_SW                   ; Move PORTA values to NEW
+
+    ;MOVFF   NEW, OLD        ; Move current state to OLD
+    ;CLRF    NEW             ; Clear NEW
+    ;BTFSC   PORTA,0         ; Check PORTA,0 --> If set write to NEW,0
+    ;BSF     NEW,0
+    ;BTFSC   PORTA,1         ; Check PORTA,1 --> If set write to NEW,1
+    ;BSF     NEW,1
+    ;BTFSC   PORTA,2         ; etc...
+    ;BSF     NEW,2
+    ;BTFSC   PORTA,3
+    ;BSF     NEW,3
+    ;BCF     FLAG, 0
+    MOVLW    b'00000111'
+    MOVFF    WREG, NEW
+    RETURN
+
+Stepper_FSM
+
+    BTFSS   NEW,0         ; ON/OFF = OFF goto main
+    goto    MAIN
+    BTFSS   NEW,1           ; MANUAL/AUTO
+    goto    MANUAL
+    goto    AUTO
+
+MANUAL
+
+AUTO
+    BCF     FLAG,1
+
 
 FSM
 
-    goto FSM
+
+
+
+
 
 inter_high
-    ADDLW   0x01
+
+    clrf    FLAG
+    btfsc   INTCON, TMR0IF
+    bsf     FLAG,0          ;Timer0 overflowed --> Switch_FSM
+    btfsc   PIR1, TMR1IF
+    bsf     FLAG,1          ;Timer1 overflowed --> Stepper_FSM
+    movff   SHIFTHIGH, TMR1H;Shift TMR1H
+    movff   SHIFTLOW, TMR1L ;Shift TMR1L
+
     BCF     INTCON, TMR0IF  ;clear IF
     BCF     PIR1,   TMR1IF  ;clear IF
+
     RETFIE
 
 inter_low
     nop
     retfie
 
-
-
-    END
+END
